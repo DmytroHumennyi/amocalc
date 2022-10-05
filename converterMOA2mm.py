ef37dce3938a99ab6a99dbe3f20c938542f6bb80
DmytroHumennyi <apollo.d.g@gmail.com>
2022-10-05 23:07:39 +0300

    Add files via upload

--- converterMOA2mm.py
+++ converterMOA2mm.py
@@ -4,44 +4,34 @@ import numpy as np
 import json
 
 
-class Amo:
-    def __init__ (self,vendor='def',calibre='0'):
+class Amo: # Amunition : balistic
+    def __init__ (self,vendor='def',calibre='223'): # Init
         self.vendor = vendor
-        self.calibre = calibre # Precondition
+        self.calibre = calibre
             
-    def save (self):
-    # create a JSON file and save ther Balistic paramets of Amunition.
-    # JSON file name: VendorCalibre
-    # Balistic parameters: Distance [Correcrion, Velicity]    
+    def save (self): # Saving the Amunition dataset  
         jsonFileName = self.vendor+self.calibre
-        if os.path.exists (jsonFileName):
-             if input (f"file {jsonFileName} esist. Would You like re-write it? (y/n): ") == 'y':
-                self.balistic = {}
-                print (f"\n New Amo : {self.vendor}, {self.calibre} \n")
-                index = 0
-                while input("Add one data to the Set? (y/n): ") != 'n':
-                    index = index + 1
-                    distance = input ("Distande [m]: ")
-                    deviation = input ("Deviation [mm]:")
-                    velicity = input ("Velocity [m/s]: ")
-                    self.balistic.update ({index:[distance, deviation,velicity]})
-                with open (jsonFileName,'w') as f:
-                    json.dump (self.balistic, f) # Saving the Amunition dataset
+        if (os.path.exists (jsonFileName)) and (input (f"file {jsonFileName} esist. Would You like re-write it? (y/n): ") == 'y') or not (os.path.exists (jsonFileName)) :
+            self.balistic = {}
+            print (f"\n New Amo : {self.vendor}, {self.calibre} \n")
+            index = 0
+            while input("Add one data to the Set? (y/n): ") != 'n':
+                index = index + 1
+                distance = input ("Distande [m]: ")
+                deviation = input ("Deviation [mm]:")
+                velicity = input ("Velocity [m/s]: ")
+                self.balistic.update ({index:[distance, deviation,velicity]})
+            with open (jsonFileName,'w') as f:
+                json.dump (self.balistic, f)
 
-    def load (self):
-    # load balistaic dataset from a JSON 
-    # JSON file name: VendorCalibre
-    # Balistic parameters: Distance [Correcrion, Velicity]    
+    def load (self): # load balistaic dataset from a JSON
         jsonFileName = self.vendor+self.calibre
         if os.path.exists (jsonFileName):
              with open (jsonFileName,'r') as f:
                  self.balistic = json.load(f)
              print ("The DataSet is loaded successfully") # Loading tha Amunition dataset
 
-    def show (self, table = True):
-    # Visualisation the balistaic dataset from a JSON 
-    # After loaing the data from JSON file
-    # it can be visualised as Table or Graph
+    def show (self, table = True): # Visualisation the balistaic dataset from a JSON
         if table == True:
              print ("Distance [m], Deviation [mm], Velocity (m/s) ")
              print (f"Amo : {self.vendor}, {self.calibre}")
@@ -49,11 +39,7 @@ class Amo:
         else:
             print ("Visualisation is disabled") # Print table of balistic (Distance, Deviation, Velocity)
     
-    def get (self, distance = 0):
-    # Getting deviation (in mm) regarding to distance of shoting.
-    # To estimate it ther is Interplation methods of colculation
-    # Param's:
-    #   distance (in meters)
+    def get (self, distance = 0): # Getting deviation (in mm) regarding to distance of shoting.
         tol = 0.01
         x = distance
         for sets in self.balistic.values():
@@ -74,17 +60,18 @@ class Amo:
         x2 = dis_h
         y1 = dev_l
         y2 = dev_h
-        y = y1 + (x-x1)*((y2-y1)/(x2-x1))    
-        return y # Getting deviation (in mm) relates to to Distance
+        z1 = vel_l
+        z2 = vel_h
 
+        y = y1 + (x-x1)*((y2-y1)/(x2-x1)) # interpolate Deviation
+        z = z1 + (x-x1)*((z2-z1)/(x2-x1)) # interpolate Velocity
+        return y
 
-
- ## BODY
-      
-
-amo = Amo ("rem","223")
+## BODY
+#amo = Amo (input ("Plase give vendor name: "))
+amo = Amo ('sts','223')
+amo.save ()
+amo.show ()
 amo.load ()
-while True:
-
-    y = float(amo.get (float(input ("Pleae give Distance: "))))
-    print (f"Correction should be:{int(y)}, [mm]")
+y = amo.get (float(input ("Pleae give Distance: ")))
+print (f"Correction should be:{int(y)}, [mm]")
